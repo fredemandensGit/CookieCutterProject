df924d25ca11ac8ca518b19c8a39576c04ba7c34
Frederik Hartmann <frederik_hartmann@hotmail.com>
2022-01-10 15:02:47 +0100

    GitHub tests aswell as additions to other tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,6 +4,8 @@
 ######################################
 # Data manipulation
 import torch
+import os
+import pytest
 
 # Graphics
 import seaborn as sns
@@ -15,6 +17,7 @@ import numpy as np
 import pdb
 
 # Load data
+from tests import _PATH_DATA
 Train = torch.load("data/processed/train_processed.pt")
 Test = torch.load("data/processed/test_processed.pt")
 
@@ -23,11 +26,17 @@ N_train = 40000
 N_test = 5000
 
 # testing
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed/test_processed.pt'), reason="Data files not found")
 def test_N_observations():
+    Train = torch.load("data/processed/train_processed.pt")
+    Test = torch.load("data/processed/test_processed.pt")
     assert len(Train) == N_train, "Train dataset does not contain expected number of observations"
     assert len(Test) == N_test, "Test dataset does not contain expected number of observations"
-    
+
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed/test_processed.pt'), reason="Data files not found")
 def test_Int_Labels():
+    Train = torch.load("data/processed/train_processed.pt")
+    Test = torch.load("data/processed/test_processed.pt")
     for i in range(len(Train)):
         assert type(Train.__getitem__(i)[1]) == torch.Tensor, "Train data not a tensor"
         assert type(Train.__getitem__(i)[1].item()) == int, "Train labels not integer"
@@ -36,11 +45,17 @@ def test_Int_Labels():
         assert type(Train.__getitem__(i)[1]) == torch.Tensor, "Test data not a tensor"
         assert type(Test.__getitem__(i)[1].item()) == int, "Test labels not integer"
 
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed/test_processed.pt'), reason="Data files not found")
 def test_Classes_Represented():
+    Train = torch.load("data/processed/train_processed.pt")
+    Test = torch.load("data/processed/test_processed.pt")
     assert (torch.unique(Train[:][1]).numpy() == np.arange(0,10)).all(), "All classes not represented in train data"
     assert (torch.unique(Test[:][1]).numpy() == np.arange(0,10)).all(), "All classes not represented in test data"
     
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed/test_processed.pt'), reason="Data files not found")
 def test_DataShape():
+    Train = torch.load("data/processed/train_processed.pt")
+    Test = torch.load("data/processed/test_processed.pt")
     for i in range(len(Train)):
         assert Train.__getitem__(i)[0].shape[0] == 28, "Image shape not correct in train data"
         assert Train.__getitem__(i)[0].shape[1] == 28, "Image shape not correct in train data"
@@ -49,3 +64,5 @@ def test_DataShape():
         assert Test.__getitem__(i)[0].shape[0] == 28, "Image shape not correct in test data"
         assert Test.__getitem__(i)[0].shape[1] == 28, "Image shape not correct in train data"
 
+def test_load_data():
+    torch.load(f'{_PATH_DATA}/processed/test_processed.pt')
---
+++ tests/test_training.py
@@ -0,0 +1,45 @@
+## -*- coding: utf-8 -*-
+######################################
+############## Imports ###############
+######################################
+# Data manipulation
+import torch
+import os
+
+# Graphics
+import seaborn as sns
+sns.set_style("whitegrid")
+
+import numpy as np
+
+# debugging
+import pdb
+import pytest
+
+# load model
+from src.models.model import MyAwesomeModel
+
+# Import path to data
+from tests import _PATH_DATA
+
+# Load data
+Train = torch.load("data/processed/train_processed.pt")
+Test = torch.load("data/processed/test_processed.pt")
+
+# Load model
+model = MyAwesomeModel()
+model.load_state_dict(torch.load("models/trained_model.pt"))
+
+#pdb.set_trace()
+
+# testing training routine
+#def test_has_zero_gradient():
+#    with pytest.raises(ValueError, match="Weights not set to zero in training loop!"):
+        # run something to capture this in training loop - no idea
+        
+
+    
+# Test loading data
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed/test_processed.pt'), reason="Data files not found")
+def test_load_data():
+    torch.load(f'{_PATH_DATA}/processed/test_processed.pt')
